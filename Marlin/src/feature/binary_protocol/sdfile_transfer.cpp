981fdfee79a31f318f31fa568c8e5a5311d5b7ab
Christopher Pepper <p3p@p3psoft.co.uk>
2019-09-08 16:39:26 +0100

    start

--- Marlin/src/feature/binary_protocol/sdfile_transfer.cpp
+++ Marlin/src/feature/binary_protocol/sdfile_transfer.cpp
@@ -20,17 +20,18 @@
  *
  */
 
-#include "../inc/MarlinConfigPre.h"
+#include "../../inc/MarlinConfigPre.h"
 
 #if ENABLED(BINARY_FILE_TRANSFER)
 
-#include "../sd/cardreader.h"
-#include "binary_protocol.h"
+#include "../../sd/cardreader.h"
+#include "sdfile_transfer.h"
 
 char* SDFileTransferProtocol::Packet::Open::data = nullptr;
 size_t SDFileTransferProtocol::data_waiting, SDFileTransferProtocol::transfer_timeout, SDFileTransferProtocol::idle_timeout;
 bool SDFileTransferProtocol::transfer_active, SDFileTransferProtocol::dummy_transfer, SDFileTransferProtocol::compression;
 
-BinaryStream binaryStream[NUM_SERIAL];
-
+SDFileTransferProtocol::Packet::ActionResponse SDFileTransferProtocol::response_data{};
+BinaryStream::PacketInfo SDFileTransferProtocol::tx_packet{};
+SDFileTransferProtocol::Packet::QueryResponse SDFileTransferProtocol::query_data{};
 #endif // BINARY_FILE_TRANSFER
